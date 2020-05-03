cf7fde5f644dfd265579d1f34b895a4a0d3f830e
David Patiashvili <stratagem.david@gmail.com>
2020-05-03 11:38:05 +0000

    Create client for request

--- src/Insee.php
+++ src/Insee.php
@@ -2,6 +2,183 @@
 
 namespace Davaxi;
 
+use GuzzleHttp\Client;
+
+/**
+ * Class Insee.
+ */
 class Insee
 {
+    const INSEE_GET_TOKEN = 'https://api.insee.fr/token';
+    const INSEE_REVOKE_TOKEN = 'https://api.insee.fr/revoke';
+    // 7 days
+    const DEFAULT_VALIDITY_PERIOD = 604800;
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $clientId;
+
+    /**
+     * @var string
+     */
+    protected $clientSecret;
+
+    /**
+     * @var array
+     */
+    protected $accessToken;
+
+    /**
+     * Insee constructor.
+     *
+     * @param null $client
+     */
+    public function __construct($client = null)
+    {
+        if (!$client) {
+            $client = new Client();
+        }
+        $this->client = $client;
+    }
+
+    /**
+     * @param string $clientId
+     */
+    public function setClientId(string $clientId)
+    {
+        $this->clientId = $clientId;
+    }
+
+    /**
+     * @param string $clientSecret
+     */
+    public function setClientSecret(string $clientSecret)
+    {
+        $this->clientSecret = $clientSecret;
+    }
+
+    /**
+     * Use Help of INSEE
+     * https://api.insee.fr/catalogue/site/themes/wso2/subthemes/insee/pages/help.jag.
+     *
+     * @param int $validityPeriod
+     *
+     * @throws \Exception
+     */
+    public function authenticate($validityPeriod = null)
+    {
+        $this->checkClientOauth();
+        if (!$validityPeriod) {
+            $validityPeriod = static::DEFAULT_VALIDITY_PERIOD;
+        }
+        $result = $this->client->request(
+            'POST',
+            static::INSEE_GET_TOKEN,
+            [
+                'form_params' => [
+                    'grant_type' => 'client_credentials',
+                    'validity_period' => $validityPeriod,
+                ],
+                'headers' => [
+                    'Authorization' => $this->getAuthorizationHeader(),
+                ],
+            ]
+        );
+
+        $accessToken = json_decode($result->getBody(), true);
+        $this->setAccessToken($accessToken);
+    }
+
+    /**
+     * @doc https://api.insee.fr/catalogue/site/themes/wso2/subthemes/insee/pages/help.jag
+     *
+     * @param array $accessToken
+     *
+     * @throws \Exception
+     */
+    public function revokeAccessToken()
+    {
+        $this->checkClientOauth();
+        $this->checkAccessToken();
+
+        $result = $this->client->request(
+            'POST',
+            static::INSEE_REVOKE_TOKEN,
+            [
+                'form_params' => [
+                    'token' => $this->getAccessTokenValue(),
+                ],
+                'headers' => [
+                    'Authorization' => $this->getAuthorizationHeader(),
+                ],
+            ]
+        );
+        $this->accessToken = null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorizationHeader() : string
+    {
+        return sprintf(
+            'Basic %s',
+            base64_encode(
+                sprintf(
+                    '%s:%s',
+                    $this->clientId,
+                    $this->clientSecret
+                )
+            )
+        );
+    }
+
+    /**
+     * @param array $accessToken
+     */
+    public function setAccessToken(array $accessToken)
+    {
+        $this->accessToken = $accessToken;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAccessToken() : array
+    {
+        return $this->accessToken ?? [];
+    }
+
+    public function getAccessTokenValue() : string
+    {
+        $accessToken = $this->getAccessToken();
+
+        return $accessToken ? $accessToken['access_token'] : '';
+    }
+
+    protected function checkAccessToken()
+    {
+        if (!$this->getAccessTokenValue()) {
+            throw new \LogicException('Missing access token');
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function checkClientOauth()
+    {
+        if (!$this->clientId) {
+            throw new \Exception('Missing client Id');
+        }
+        if (!$this->clientSecret) {
+            throw new \Exception('Missing client secret');
+        }
+    }
 }
--- tests/InseeTest.php
+++ tests/InseeTest.php
@@ -25,4 +25,98 @@ class InseeTest extends InseePHPUnit
     {
         $this->assertTrue(true);
     }
+
+    public function testAuthenticate_withoutClientIdSecret()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client Id");
+        $this->insee->authenticate();
+    }
+
+    public function testAuthenticate_withoutClientId()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client Id");
+        $this->insee->setClientSecret('CLIENT_SECRET');
+        $this->insee->authenticate();
+    }
+
+    public function testAuthenticate_withoutClientSecret()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client secret");
+        $this->insee->setClientId('CLIENT_ID');
+        $this->insee->authenticate();
+    }
+
+    public function testAuthenticate_withInvalidOauth()
+    {
+        $this->expectException("GuzzleHttp\Exception\ClientException");
+        $this->insee->setClientId('invalid_client_id');
+        $this->insee->setClientSecret('invalid_client_secret');
+        $this->insee->authenticate();
+    }
+
+    public function testAuthenticate()
+    {
+        $this->insee->setClientId($_ENV['CLIENT_ID']);
+        $this->insee->setClientSecret($_ENV['CLIENT_SECRET']);
+        $this->insee->authenticate(10);
+
+        $accessToken = $this->insee->getAccessToken();
+        $this->assertInternalType('array', $accessToken);
+        $this->assertArrayHasKey('access_token', $accessToken);
+        $this->assertNotNull($accessToken['access_token']);
+        $this->assertArrayHasKey('scope', $accessToken);
+        $this->assertNotNull($accessToken['scope']);
+        $this->assertArrayHasKey('token_type', $accessToken);
+        $this->assertEquals('Bearer', $accessToken['token_type']);
+        $this->assertArrayHasKey('expires_in', $accessToken);
+    }
+
+    public function testRevokeAccessToken_withoutClientIdSecret()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client Id");
+        $this->insee->revokeAccessToken();
+    }
+
+    public function testRevokeAccessToken_withoutClientId()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client Id");
+        $this->insee->setClientSecret('CLIENT_SECRET');
+        $this->insee->revokeAccessToken();
+    }
+
+    public function testRevokeAccessToken_withoutClientSecret()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing client secret");
+        $this->insee->setClientId('CLIENT_ID');
+        $this->insee->revokeAccessToken();
+    }
+
+    public function testRevokeAccessToken_withoutAccessToken()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage("Missing access token");
+        $this->insee->setClientId('invalid_client_id');
+        $this->insee->setClientSecret('invalid_client_secret');
+        $this->insee->revokeAccessToken();
+    }
+
+    public function testRevoke()
+    {
+        $this->insee->setClientId($_ENV['CLIENT_ID']);
+        $this->insee->setClientSecret($_ENV['CLIENT_SECRET']);
+        $this->insee->authenticate(10);
+
+        $accessToken = $this->insee->getAccessToken();
+        $this->assertInternalType('array', $accessToken);
+
+        $this->insee->revokeAccessToken();
+        $accessToken = $this->insee->getAccessToken();
+        $this->assertEmpty($accessToken);
+    }
 }
